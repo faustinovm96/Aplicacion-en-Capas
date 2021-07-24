0e2a22ffee1b216ed0942c8bc36f74db72360983
faustinovm96 <fvillasboa96@gmail.com>
2021-07-23 21:13:58 -0400

    Primeras Pruebas de las capas

---
+++ src/main/java/py/com/hw/test/TestCategorias.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.com.hw.test;
+
+import py.com.hw.modelo.jdbc.Categoria;
+import py.com.hw.service.CategoriaService;
+
+/**
+ *
+ * @author F996
+ */
+public class TestCategorias {
+    public static void main(String[] args) {
+        Categoria cat = new Categoria();
+//        cat.setCategoria("TINTAS");
+//        cat.setDescripcion("Tintas para impresoras");
+//        
+        CategoriaService catService = new CategoriaService();
+//        cat = catService.findCategoria(2);
+//        cat.setDescripcion("TINTA PARA IMPRESORAS Y SUBLIMADORAS");
+//        
+//        if(catService.updateCategoria(cat)){
+//            System.out.println("Cambios Guardados Correctamente");
+//        }else{
+//            System.out.println("ERROR");
+//        }
+//        if(catService.saveCategoria(cat)) System.out.println("Registro Exitoso");
+
+//          System.out.println("Lista"+ catService.findAllCategorias());
+            if(catService.deleteCategoria(2)) System.out.println("Registro Eliminado");
+    }
+}
