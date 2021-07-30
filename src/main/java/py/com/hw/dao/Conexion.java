32c609d7d5e61a30f8128a4134b930d484da2e1d
faustinovm96 <fvillasboa96@gmail.com>
2021-07-30 19:53:24 -0400

    Implementacion de la Clase de Conexion Cambiada

--- src/main/java/py/com/hw/dao/Conexion.java
+++ src/main/java/py/com/hw/dao/Conexion.java
@@ -1,13 +1,11 @@
 package py.com.hw.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import py.com.hw.dao.util.SystemConstants;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  * Clase de conexión a la base de datos
@@ -17,33 +15,76 @@ import py.com.hw.dao.util.SystemConstants;
  */
 public class Conexion {
 
-    private static Conexion instance;
-    private Connection connection;
-    
-    private Conexion() throws SQLException {
-        jdbcConnection();
+//    private static Conexion instance;
+//    private Connection connection;
+//    
+//    private Conexion() throws SQLException {
+//        jdbcConnection();
+//    }
+//
+//    private void jdbcConnection() throws SQLException {
+//        try {
+//            Class.forName(SystemConstants.DRIVER);
+//            this.connection = DriverManager.getConnection(SystemConstants.URL, SystemConstants.USERNAME, SystemConstants.PASSWORD);
+//        } catch (ClassNotFoundException ex) {
+//            System.out.println("Problema Ocurrido : " + ex.getMessage());
+//        }
+//    }
+//
+//    public Connection getConnection() {
+//        return connection;
+//    }
+//
+//    public static Conexion getInstance() throws SQLException {
+//        if (instance == null) {
+//            System.out.println("Pasa Por AQUI SINGLETON");
+//            instance = new Conexion();
+//        } else if (instance.getConnection().isClosed()) {
+//            instance = new Conexion();
+//        }
+//        return instance;
+//    }
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test_1?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "holamundo96";
+    private static BasicDataSource dataSource;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl(JDBC_URL);
+            dataSource.setUsername(JDBC_USER);
+            dataSource.setPassword(JDBC_PASS);
+            dataSource.setInitialSize(50);
+        }
+        return dataSource;
     }
 
-    private void jdbcConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet rs) {
         try {
-            Class.forName(SystemConstants.DRIVER);
-            this.connection = DriverManager.getConnection(SystemConstants.URL, SystemConstants.USERNAME, SystemConstants.PASSWORD);
-        } catch (ClassNotFoundException ex) {
-            System.out.println("Problema Ocurrido : " + ex.getMessage());
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
         }
     }
 
-    public Connection getConnection() {
-        return connection;
+    public static void close(PreparedStatement stmt) {
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 
-    public static Conexion getInstance() throws SQLException {
-        if (instance == null) {
-            System.out.println("Pasa Por AQUI SINGLETON");
-            instance = new Conexion();
-        } else if (instance.getConnection().isClosed()) {
-            instance = new Conexion();
+    public static void close(Connection conn) {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
         }
-        return instance;
     }
 }
